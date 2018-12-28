5fcc620dd6bfcc784b273eba961b29a3655e8cf1
ryanszu99 <46079413+ryanszu99@users.noreply.github.com>
2018-12-28 20:29:10 +0800

    Update class.py

--- class.py
+++ class.py
@@ -55,14 +55,14 @@ class Firm:
 	
 		self.BS , self.IS, self.CS = df_list
 	def getratios(self):
-		#ROE
+				#ROE
 		try:
 			self.ave_equity =  int(  self.BS[self.BS.item.isin( ['權益總額'] ) ].values[0][1] )+ int(  self.BS[self.BS.item.isin( ['權益總額'] ) ].values[0][2] )
 			self.NI =    int(  self.IS[self.IS.item.isin( ['本期綜合損益總額'] ) ].values[0][1] )  
 			self.ROE = self.NI/self.ave_equity 
 		except:
 			self.ROE='n/a'
-		print(self.ROE)
+		print('ROE=',self.ROE)
 
 		#流動比率
 		try:
@@ -71,43 +71,43 @@ class Firm:
 			self.current_ratio=self.current_assets/self.current_liabilities
 		except:
 			self.current_ratio='n/a'
-		print(self.current_ratio)
+		print('流動比率=',self.current_ratio)
 
 		#速動比率
 		try:
-			self.current_assets =  int(  self.BS[self.BS.item.isin( ['流動資產合計'] ) ].values[0][1] ) 
-			self.current_liabilities =  int(  self.BS[self.BS.item.isin( ['流動負債合計'] ) ].values[0][1] ) 
-			self.inventory =    int(  self.BS[self.BS.item.isin( ['存貨合計''存貨'] ) ].values[0][1] )
+#			self.current_assets =  int(  self.BS[self.BS.item.isin( ['流動資產合計'] ) ].values[0][1] ) 
+#			self.current_liabilities =  int(  self.BS[self.BS.item.isin( ['流動負債合計'] ) ].values[0][1] ) 
+			self.inventory =    int(  self.BS[self.BS.item.isin( ['存貨','存貨合計'] ) ].values[0][1] )
 			self.Accounts_receivable =    int(  self.BS[self.BS.item.isin( ['應收帳款淨額'] ) ].values[0][1] )
-			self.speed_ratio=self.current_assets-self.inventory-self.Accounts_receivable/self.current_liabilities
+			self.speed_ratio=(self.current_assets-self.inventory-self.Accounts_receivable)/self.current_liabilities
 		except:
 			self.speed_ratio='n/a'
-		print(self.speed_ratio)
-			#資產負債率
+		print('速動比率=',self.speed_ratio)
+		#資產負債率
 		try:
-			self.total_liabilities =  int(  self.BS[self.BS.item.isin( ['負債總額'] ) ].values[0][1] ) 
-			self.total_assets =    int(  self.BS[self.BS.item.isin( ['資產總額'] ) ].values[0][1] )
+			self.total_liabilities =  int(  self.BS[self.BS.item.isin( ['負債總額','負債總計'] ) ].values[0][1] ) 
+			self.total_assets =    int(  self.BS[self.BS.item.isin( ['資產總額','資產總計'] ) ].values[0][1] )
 			self.debt_asset_ratio=self.total_liabilities/self.total_assets
 		except:
 			self.debt_asset_ratio='n/a'
-		print(self.debt_asset_ratio)
+		print('資產負債率=',self.debt_asset_ratio)
 		#股東權益比率
 		try:
 			self.Total_shareholders_equity =  int(  self.BS[self.BS.item.isin( ['權益總額'] ) ].values[0][1] ) 
-			self.total_assets =    int(  self.BS[self.BS.item.isin( ['資產總額'] ) ].values[0][1] )
+#			self.total_assets =    int(  self.BS[self.BS.item.isin( ['資產總額'] ) ].values[0][1] )
 			self.Equity_ratio=self.Total_shareholders_equity/self.total_assets
 		except:
 			self.Equity_ratio='n/a'
-		print(self.Equity_ratio)
+		print('股東權益比率=',self.Equity_ratio)
 			
 		#負債與股東權益比率
 		try:
-			self.Total_shareholders_equity =  int(  self.BS[self.BS.item.isin( ['權益總額'] ) ].values[0][1] ) 
-			self.total_liabilities =  int(  self.BS[self.BS.item.isin( ['負債總額'] ) ].values[0][1] ) 
+#			self.Total_shareholders_equity =  int(  self.BS[self.BS.item.isin( ['權益總額'] ) ].values[0][1] ) 
+#			self.total_liabilities =  int(  self.BS[self.BS.item.isin( ['負債總額'] ) ].values[0][1] ) 
 			self.debt_equity_ratio=self.total_liabilities/self.Total_shareholders_equity
 		except:
 			self.debt_equity_ratio='n/a'
-		print(self.debt_equity_ratio)
+		print('負債與流動比率=',self.debt_equity_ratio)
 		#毛利率
 		try:
 			self.net_revenue =  int(  self.IS[self.IS.item.isin( ['營業毛利（毛損）'] ) ].values[0][1] ) 
@@ -115,18 +115,18 @@ class Firm:
 			self.Gross_margin=self.net_revenue/self.revenue
 		except:
 			self.Gross_margin='n/a'
-		print(self.Gross_margin)
+		print('毛利率=',self.Gross_margin)
 			
 			
 		#營業利益率
 		try:
-			self.revenue =  int(  self.IS[self.IS.item.isin( ['營業收入合計'] ) ].values[0][1] ) 
-			self.Operating_Income =  int(  self.IS[self.IS.item.isin( ['營業利益(合計)'] ) ].values[0][1] ) 
-			self.revenue =  int(  self.IS[self.IS.item.isin( ['營業收入合計'] ) ].values[0][1] ) 
+#			self.revenue =  int(  self.IS[self.IS.item.isin( ['營業收入合計'] ) ].values[0][1] ) 
+			self.Operating_Income =  int(  self.IS[self.IS.item.isin( ['營業利益(合計)','營業利益（損失）'] ) ].values[0][1] ) 
 			self.Operating_Profit_Margin=self.Operating_Income/self.revenue
 		except:
 			self.Operating_Profit_Margin='n/a'
-		print(self.Operating_Profit_Margin)
+		print('營業利益率=',self.Operating_Profit_Margin)
+		
 		
 		
 
